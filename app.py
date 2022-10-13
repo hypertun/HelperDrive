fa949a598e3ea3e2d3d447d12858960abf1181ac
Ivan_Yeo <ivan.yeo@ntucenterprise.sg>
2022-10-13 10:06:12 +0800

    added templates folder

--- app.py
+++ app.py
@@ -22,13 +22,13 @@
 #  
 #  
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello! Felicia!</h1>"
+    return render_template('templates/addnewmaid.html')
 
 def create_app():
    return app
