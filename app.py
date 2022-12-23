cb503485fe5e1b0d2b554af3b60186c4d540be8c
Ivan <ivan.yeo@ntucenterprise.sg>
2022-12-23 17:50:07 +0800

    added edit button and screen

--- app.py
+++ app.py
@@ -22,16 +22,19 @@
 #
 #
 
+from datetime import datetime
 from pydoc import Helper
-from flask import Flask, render_template, request, flash,url_for
+from flask import Flask, render_template, request, flash, url_for
 from flaskext.mysql import MySQL
+from classes import helpers
 from classes.helpers import Helper
 import os
 
 app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'webadmin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get(
+    'MYSQL_DATABASE_PASSWORD')
 app.config['MYSQL_DATABASE_DB'] = 'helper_drive'
 app.config['MYSQL_DATABASE_HOST'] = '116.86.63.183'
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -41,80 +44,69 @@ mysql.init_app(app)
 @app.route("/")
 def index():
     conn = mysql.connect()
-    data = getAllHelpers(conn)
+    data = helpers.getAllHelpers(conn)
     conn.close()
-    return render_template('helpers.html', listOfHelpers=data,pages=generate_page_list())
+    return render_template('helpers.html', listOfHelpers=data, pages=generate_page_list())
 
 
-@app.route("/addHelper", methods=["GET", "POST"])
-def addHelper():
+@app.route("/addEditHelper",defaults={'id': None}, methods=["GET", "POST"])
+@app.route("/addEditHelper/<id>", methods=["GET", "POST"])
+def addEditHelper(id):
     if request.method == "GET":
-        return render_template("addHelper.html",pages=generate_page_list())
+        if id is None:
+            return render_template("addEditHelper.html", pages=generate_page_list(),helper=None)
+        else:
+            conn = mysql.connect()
+            helper = Helper("","","","","","")
+            helper.id = id
+            fullDetails = helper.get(conn)
+            conn.close()
+            return render_template("addEditHelper.html", pages=generate_page_list(),helper=fullDetails)
     elif request.method == "POST":
         conn = mysql.connect()
-        name,code,status,nationality,medical,staffId,err = validationForAdd(request)
+        name, code, status, nationality, medical, staffId, err = validationForAdd(request)
+        id, dob, arrivalDate, flightNo, fin = validationForUpdate(request)
         if not err:
-            addHelper(conn, name, code, status, nationality, medical, staffId)
+             helper = addEditHelper(conn,id, name, code, status, nationality, medical,
+                          staffId, dob, arrivalDate, flightNo, fin)
         conn.close()
-        return render_template("addHelper.html",pages=generate_page_list())
+        return render_template("addEditHelper.html", pages=generate_page_list(),helper=helper)
 
 
 @app.route("/delHelper", methods=["GET", "POST"])
 def delHelper():
     if request.method == "GET":
-        return render_template("delHelper.html",pages=generate_page_list())
+        return render_template("delHelper.html", pages=generate_page_list())
     elif request.method == "POST":
         conn = mysql.connect()
         id = request.form["id"]
-        print(id)
         delHelper(conn, id)
         conn.close()
-        return render_template("delHelper.html",pages=generate_page_list())
+        return render_template("delHelper.html", pages=generate_page_list())
 
 
 def create_app():
     return app
 
 
-def getAllHelpers(conn):
-    cursor = conn.cursor()
-
-    helperList = []
-    cursor.execute("SELECT * from helpers")
-    data = cursor.fetchall()
-
-    for eachHelper in data:
-        id, name, code, dob, arrival_Date, flight_No, status, nationality, fin, medical, staff_id, created_at, updated_at = eachHelper
-        newHelper = Helper(name,
-                           code,
-                           status,
-                           nationality,
-                           medical,
-                           staff_id,
-                           )
-        newHelper.id = id
-        newHelper.dob = dob
-        newHelper.arrivalDate = arrival_Date
-        newHelper.flightNo = flight_No
-        newHelper.fin = fin
-        newHelper.createdAt = created_at
-        newHelper.updatedAt = updated_at
-
-        helperList.append(newHelper)
-
-    cursor.close()
-    return helperList
-
-
-def addHelper(conn, name, code, status, nationality, medical, staffId):
+def addEditHelper(conn,id, name, code, status, nationality, medical, staffId, dob, arrivalDate, flightNo, fin):
     newHelper = Helper(name=name, code=code, status=status,
                        nationality=nationality, medical=medical, staff_Id=staffId)
-    newHelper.add(conn)
+    newHelper.date_of_birth = dob
+    newHelper.arrival_date = arrivalDate
+    newHelper.flight_no = flightNo
+    newHelper.fin = fin
+    if id is None:
+        newHelper.add(conn)
+    else:
+        newHelper.id = id
+        newHelper.edit(conn)
+    return newHelper
 
 
 def delHelper(conn, id):
-    newHelper = Helper(name="May Fen Tei", code="Aw 1276", status="fresh",
-                       nationality="Myanmar", medical="pass", staff_Id=1)
+    newHelper = Helper(name="", code="", status="",
+                       nationality="", medical="", staff_Id=1)
     newHelper.id = id
     newHelper.delete(conn)
 
@@ -122,26 +114,51 @@ def delHelper(conn, id):
 def validationForAdd(request):
     error = False
     name = request.form["name"]
+    if not name:
+        flash('Name is required!')
+        error = True
     code = request.form["code"]
+    if not code:
+        flash('Code is required!')
+        error = True
     status = request.form["status"].casefold()
     if not status or (status != "fresh" and status != "transfer"):
         flash('Status is required as fresh/transfer!')
-        error=True
+        error = True
     nationality = request.form["nationality"].casefold()
     if not nationality or (nationality != "indonesia" and nationality != "myanmar" and nationality != "philippines"):
         flash('Nationality is required as Indonesia/Myanmar/Philippines!')
-        error=True
+        error = True    
     medical = request.form["medical"].casefold()
     if not medical or (medical != "pass" and medical != "fail"):
         flash('Medical is required as pass/fail!')
-        error=True
+        error = True
     staffId = request.form["staff_Id"]
-    return name,code,status,nationality,medical,staffId,error
+    if not code:
+        flash('StaffId is required!')
+        error = True
+
+    return name, code, status, nationality, medical, staffId, error
+
+
+def validationForUpdate(request):
+    id = request.form["id"]
+    dob = request.form["dob"]
+    if dob is not "":
+        dob = datetime.strptime(dob, '%Y-%m-%d')
+    arrivalDate = request.form["arrivalDate"]
+    if arrivalDate is not "":
+        arrivalDate = datetime.strptime(arrivalDate,'%Y-%m-%dT%H:%M')
+    flightNo = request.form["flightNo"]
+    fin = request.form["fin"]
+
+    return id,dob, arrivalDate, flightNo, fin
+
 
 def generate_page_list():
     pages = [
         {"name": "HelperList", "url": url_for("index")},
-        {"name": "Add", "url": url_for("addHelper")},
-        {"name": "Del", "url": url_for("delHelper")}, 
+        {"name": "Add", "url": url_for("addEditHelper")},
+        {"name": "Del", "url": url_for("delHelper")},
     ]
     return pages
--- classes/helpers.py
+++ classes/helpers.py
@@ -2,21 +2,48 @@
 from datetime import date
 
 
+def getAllHelpers(conn):
+    helperList = []
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from helpers")
+    data = cursor.fetchall()
+    for eachHelper in data:
+        id, name, code, dob, arrival_Date, flight_No, status, nationality, fin, medical, staff_id, created_at, updated_at = eachHelper
+        newHelper = Helper(name,
+                       code,
+                       status,
+                       nationality,
+                       medical,
+                       staff_id,
+                       )
+        newHelper.id = id
+        newHelper.date_of_birth = dob
+        newHelper.arrival_date = arrival_Date
+        newHelper.flight_no = flight_No
+        newHelper.fin = fin
+        newHelper.created_at = created_at
+        newHelper.updated_at = updated_at
+        helperList.append(newHelper)
+
+    cursor.close()
+    return helperList
+
+
 class Helper:
     def __init__(self, name, code, status, nationality, medical, staff_Id):
         self.name = name
         self.id = -1
-        self.dob = date.today()
-        self.arrivalDate = date.today()
+        self.date_of_birth = date.today().strftime("%Y-%m-%d")
+        self.arrival_date = date.today().strftime("%Y-%m-%dT%H:%M")
         self.status = status
         self.nationality = nationality
         self.code = code
-        self.flightNo = ""
+        self.flight_no = ""
         self.fin = ""
         self.medical = medical
-        self.staffId = staff_Id
-        self.createdAt = date.today()
-        self.updatedAt = date.today()
+        self.staff_id = staff_Id
+        self.created_at = date.today().strftime("%Y-%m-%dT%H:%M")
+        self.updated_at = date.today().strftime("%Y-%m-%dT%H:%M")
 
     def add(self, conn):
         cursor = conn.cursor()
@@ -27,6 +54,49 @@ class Helper:
 
     def delete(self, conn):
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM helpers WHERE id=%s",self.id)
+        cursor.execute("DELETE FROM helpers WHERE id=%s", self.id)
         conn.commit()
         cursor.close()
+
+    def edit(self, conn):
+        cursor = conn.cursor()
+        update = "UPDATE helpers SET "
+        arguments = ()
+        for key, value in self.__dict__.items():
+            if key == "id":
+                continue
+            if value is not "":
+                update = update + key + "=%s,"  
+                arguments = arguments + (value,)
+        #remove comma
+        update = update[:-1] + " WHERE id=%s"
+        arguments = arguments + (self.id,)
+        cursor.execute(update, arguments)
+        conn.commit()
+        cursor.close()
+
+    def get(self, conn):
+        cursor = conn.cursor()
+        cursor.execute("SELECT * from helpers WHERE id=%s", self.id)
+        row = cursor.fetchone()
+        if row is None:
+            return None
+        
+        id, name, code, dob, arrival_Date, flight_No, status, nationality, fin, medical, staff_id, created_at, updated_at = row
+        newHelper = Helper(name,
+                       code,
+                       status,
+                       nationality,
+                       medical,
+                       staff_id,
+                       )
+        newHelper.id = id
+        newHelper.date_of_birth = dob
+        newHelper.arrival_date = arrival_Date
+        newHelper.flight_no = flight_No
+        newHelper.fin = fin
+        newHelper.created_at = created_at
+        newHelper.updated_at = updated_at
+
+        cursor.close()
+        return newHelper
